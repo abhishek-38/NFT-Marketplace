eb7debf6d35a6991db7692e2975191f18cbf86f2
Abhishek Tewary <abhishek.19je0038@gmail.com>
2023-09-09 15:11:47 +0530

    commit

---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    goerli: {
+      url: REACT_APP_ALCHEMY_API_URL,
+      accounts: [REACT_APP_PRIVATE_KEY],
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
